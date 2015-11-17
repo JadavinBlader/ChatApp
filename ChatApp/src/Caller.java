b33c0e64f14b6c727f6e2ffeec08420b6307ad9b
dmitr_000 <dmitr_000@dmitrii>
2015-11-17 21:39:36 +0200

    Методы класса Caller

---
+++ ChatApp/src/Caller.java
@@ -0,0 +1,41 @@
+package ChatApp.src;
+
+import java.net.SocketAddress;
+
+
+public class Caller {
+	private String nick;
+	private String ip;
+	private SocketAddress remoteAddress;
+	public Caller(){
+		
+	}
+	public Caller(String nick){
+		
+	}
+	public Caller(String nick,
+		      SocketAddress remoteAddress){
+		
+	}
+	public Caller(String nick,
+		      String ip){
+		
+	}
+	public Connection call()
+            throws java.io.IOException{
+				return null;
+		
+	}
+	public java.lang.String getLocalNick(){
+		return nick;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+	
+	
+	

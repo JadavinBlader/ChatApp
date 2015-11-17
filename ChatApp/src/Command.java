a9ba4dc486e5c5435240fd95e9c69f3821614050
Пользователь <@Jadavin_Blader.connectify>
2015-11-17 18:19:46 +0200

    Создал Command с полем protected CommandType type, конструктором public Command(CommandType t) и методом public String toString().

---
+++ ChatApp/src/Command.java
@@ -0,0 +1,17 @@
+public class Command {
+
+	protected CommandType type;
+
+	public static enum CommandType {
+		ACCEPT, DISCONNECT, MESSAGE, NICK, REJECT;
+	}
+
+	public Command(CommandType t) {
+		type = t;
+	}
+
+	public String toString(){
+		return type.toString();
+	}
+
+}

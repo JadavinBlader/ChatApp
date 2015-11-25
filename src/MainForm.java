82eb336f14af02d4be6a7452309c4997a64e76de
NickaDeutsche <veronikadeutsche@gmail.com>
2015-11-25 19:33:10 +0200

    Поправки в MainForm, пока без компонентов

--- src/MainForm.java
+++ src/MainForm.java
@@ -15,40 +15,34 @@ public class MainForm{
   
 
 
-	public static void main(String[] args) throws java.io.IOException{
+	public static void main(String[] args) throws IOException{
 		EventQueue.invokeLater(new Runnable() {
 			public void run (){
-				MainFrame frame = new MainFrame();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				//frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-		        //frame.setIconImage(icon);
-				//frame.setBounds(150, 150, DEFAULT_WIDTH, DEFAULT_HEIGHT);
-				//frame.setLocationByPlatform(true);
-				frame.setTitle("ChatApp");
-				frame.setVisible(true);
-				frame.setResizable(false);
+				MainForm frame;
+				try {
+					frame = new MainForm();
+					frame.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.frame.setTitle("ChatApp");
+					frame.frame.setVisible(true);
+					frame.frame.setResizable(true);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
 			}
 		});	
 	}
 
-}
- class MainFrame extends JFrame{
-
-
-	public MainFrame(){
-	 Toolkit kit = Toolkit.getDefaultToolkit();
+public MainForm() throws IOException {
+	frame = new JFrame();
+	frame.setBounds(150, 150, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	Toolkit kit = Toolkit.getDefaultToolkit();
 	 Dimension screenSize = kit.getScreenSize();
 	 int screenHeight = screenSize.height;
 	 int screenWidth = screenSize.width;
-	 setSize(screenWidth/2, screenHeight/2);
-	 setLocationByPlatform(true);
+	 //frame.setSize(screenWidth/2, screenHeight/2);
+	 //frame.setLocationByPlatform(true);
 	 Image img = new ImageIcon("ChatApp.png").getImage();
-	 setIconImage(img);
-	 }
-	 
+	 frame.setIconImage(img);
  }
- 
-class MyComponent extends JComponent{
-	
 }
-

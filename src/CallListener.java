25d87fffa4f880a4289d4a8c29dd69c1953c1c4a
DmitriiZinevich <dmitrii.zinevich@mail.ru>
2015-12-01 23:35:38 +0200

    Доработки

--- src/CallListener.java
+++ src/CallListener.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -12,60 +11,69 @@ public class CallListener {
 	private ServerSocket sSocket;
 	private SocketAddress Address, rAddress;
 
-
-	public CallListener() throws IOException{
+	public CallListener() throws IOException {
 		this("noNick", null);
 	}
-	public CallListener(String Lnick) throws IOException{
+
+	public CallListener(String Lnick) throws IOException {
 		this(Lnick, null);
 	}
-	public CallListener(String Lnick,
-            String Lip) throws IOException{
+
+	public CallListener(String Lnick, String Lip) throws IOException {
 		sSocket = new ServerSocket(28411);
 		if (localIp != null)
 			sSocket.bind(new InetSocketAddress(localIp, 28411));
 		this.Lnick = Lnick;
 		this.Address = sSocket.getLocalSocketAddress();
 	}
-	public Connection getConnection()
-            throws java.io.IOException{
-				return new Connection(sSocket.accept(),Lnick );
-		
+
+	public Connection getConnection() throws java.io.IOException {
+		return new Connection(sSocket.accept(), Lnick);
+
 	}
-	public String getLocalNick(){
+
+	public String getLocalNick() {
 		return Lnick;
-		
+
 	}
-	public boolean isBusy(){
+
+	public boolean isBusy() {
 		return false;
-		
+
 	}
-	public SocketAddress getListenAddress(){
+
+	public SocketAddress getListenAddress() {
 		return Address;
-		
+
 	}
-	public String getRemoteNick(){
+
+	public String getRemoteNick() {
 		return Rnick;
-		
+
 	}
-	public SocketAddress getRemoteAddress(){
+
+	public SocketAddress getRemoteAddress() {
 		return rAddress;
-		
+
 	}
-	public void setLocalNick(String Lnick){
+
+	public void setLocalNick(String Lnick) {
 		this.Lnick = Lnick;
 	}
-	public void setBusy(boolean busy){        	
-		
+
+	public void setBusy(boolean busy) {
+
 	}
-	public void setListenAddress(SocketAddress listenAddress){
+
+	public void setListenAddress(SocketAddress listenAddress) {
 		this.Address = Address;
 	}
-/*	public String toString(){
-		return Lnick ;
-		
-	}
-*/
+
+	/*
+	 * public String toString(){ return Lnick ;
+	 * 
+	 * }
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/Caller.java
+++ src/Caller.java
@@ -1,10 +1,8 @@
 
-
 import java.lang.invoke.CallSite;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
-
 
 public class Caller {
 	private String Lnick;
@@ -12,65 +10,70 @@ public class Caller {
 	private String ip;
 	private CallStatus s;
 	private SocketAddress remoteAddress;
-	
+
 	public static enum CallStatus {
-		 OK, REJECTED, BUSY, NO_SERVICE, NOT_ACCESSIBLE
+		OK, REJECTED, BUSY, NO_SERVICE, NOT_ACCESSIBLE
 	}
-	public Caller(){
+
+	public Caller() {
 		this("noNick", new InetSocketAddress("127.0.0.1", 28411));
 	}
-	public Caller(String Lnick){
+
+	public Caller(String Lnick) {
 		this(Lnick, new InetSocketAddress("127.0.0.1", 28411));
 	}
-	public Caller(String Lnick,
-		      SocketAddress remoteAddress){
-		this.remoteAddress=remoteAddress;
-		this.Lnick=Lnick;
+
+	public Caller(String Lnick, SocketAddress remoteAddress) {
+		this.remoteAddress = remoteAddress;
+		this.Lnick = Lnick;
 	}
-	public Caller(String Lnick,
-		      String ip){
+
+	public Caller(String Lnick, String ip) {
 		this(Lnick, new InetSocketAddress(ip, 28411));
 	}
-	public Connection call()
-            throws java.io.IOException{
+
+	public Connection call() throws java.io.IOException {
 		Socket socket = new Socket();
 		socket.bind(new InetSocketAddress(socket.getLocalAddress(), 28411));
 		socket.connect(remoteAddress);
-		return new Connection(socket,Lnick);
-			
-		
+		return new Connection(socket, Lnick);
+
 	}
-	public String getLocalNick(){
+
+	public String getLocalNick() {
 		return Lnick;
-		
+
 	}
-	public SocketAddress getRemoteAddress(){
-		return remoteAddress;		
+
+	public SocketAddress getRemoteAddress() {
+		return remoteAddress;
 	}
-	public String getRemoteNick(){
+
+	public String getRemoteNick() {
 		return Rnick;
-		
+
 	}
-	public CallStatus getStatus(){
-		return s;		
+
+	public CallStatus getStatus() {
+		return s;
 	}
-	public void setLocalNick(String Lnick){
+
+	public void setLocalNick(String Lnick) {
 		this.Lnick = Lnick;
 	}
-	public void setRemoteAddress(SocketAddress remoteAddress){		
+
+	public void setRemoteAddress(SocketAddress remoteAddress) {
 		this.remoteAddress = remoteAddress;
 	}
-/*	public String toString(){
-		return s.toString();
-		
-	}
- */
+
+	/*
+	 * public String toString(){ return s.toString();
+	 * 
+	 * }
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
 }
-	
-	
-	

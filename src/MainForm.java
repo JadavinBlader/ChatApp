30b653e8815285b92ab8bcf82a4b0a7e2a9d87f7
NickaDeutsche <veronikadeutsche@gmail.com>
2015-12-10 22:34:44 +0200

    Готовый Frame без логики

--- src/MainForm.java
+++ src/MainForm.java
@@ -4,25 +4,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import java.io.*;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
 
 public class MainForm{
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField nickField;
-	private JTextField remoteLoginField;
-	private JTextField remoteAddressField;
-	private JTextArea messageArea;
-	private JButton sendButton;
-	private JButton disconnectButton;
-	private CallListener callListener;
-	private JButton connectButton;
-	private Caller caller;
-	private Connection connection;
-	private CallListenerThread callLT;
-	private CommandListenerThread commandLT;
-	private Connection socket;
-	private static final int DEFAULT_WIDTH = 700;
-	private static final int DEFAULT_HEIGHT = 500;
+	private static JFrame frame;
+	private static JButton connectButton;
+	private static JTextField remoteNickField;
+	private static JTextField remoteAdressField;
+	private static JButton disconnectButton;
+	private static JTextField localNickField;
+	private static JTextField messageField;
+	private static JButton applyButton;
+	private static JButton sendButton;
+	private static JTextArea dialogArea;
+	private static JSeparator topSeparator;
+	private static JSeparator bottomSeparator;
 	
   
 
@@ -30,210 +30,142 @@ public class MainForm{
 	public static void main(String[] args) throws IOException{
 		EventQueue.invokeLater(new Runnable() {
 			public void run (){
-				MainForm frame;
-				try {
-					frame = new MainForm();
-					frame.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.frame.setTitle("ChatApp v1.0.0");
-					frame.frame.setVisible(true);
-					frame.frame.setResizable(true);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				frame = new JFrame();
+				frame.setLocationByPlatform(true);
+				frame.setMinimumSize(new Dimension(800, 500));
+				frame.setMaximumSize(new Dimension(1200, 800));
+				frame.setVisible(true);
+				frame.setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\OOP\\Brazauskaite_Veronika\\workspace\\ChatApp\\ChatApp.png"));
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setTitle("ChatApp v1.0.0");
+				frame.setResizable(false);
+				frame.getContentPane().setLayout(null);
+				
+				JPanel topPanel = new JPanel();
+				topPanel.setBounds(2, 0, 790, 100);
+				frame.getContentPane().add(topPanel);
+				topPanel.setLayout(null);
+				
+				connectButton = new JButton("Connect");
+				connectButton.setRequestFocusEnabled(false);
+				connectButton.setBackground(new Color(255, 215, 0));
+				connectButton.setForeground(new Color(51, 204, 51));
+				connectButton.setFont(new Font("Broadway", Font.PLAIN, 14));
+				connectButton.setToolTipText("Click to start chat");
+				connectButton.setBounds(596, 17, 145, 22);
+				topPanel.add(connectButton);
+				
+				disconnectButton = new JButton("Disconnect");
+				disconnectButton.setRequestFocusEnabled(false);
+				disconnectButton.setBackground(new Color(255, 215, 0));
+				disconnectButton.setForeground(new Color(255, 0, 0));
+				disconnectButton.setFont(new Font("Broadway", Font.PLAIN, 14));
+				disconnectButton.setToolTipText("Click to finish chat");
+				disconnectButton.setBounds(596, 51, 145, 22);
+				topPanel.add(disconnectButton);
+				
+				remoteNickField = new JTextField();
+				remoteNickField.setCaretColor(new Color(0, 0, 0));
+				remoteNickField.setToolTipText("Nick of interlocutor");
+				remoteNickField.setFont(new Font("Dialog", Font.PLAIN, 12));
+				remoteNickField.setForeground(new Color(0, 0, 0));
+				remoteNickField.setBounds(420, 17, 150, 22);
+				topPanel.add(remoteNickField);
+				remoteNickField.setColumns(15);
+				
+				remoteAdressField = new JTextField();
+				remoteAdressField.setCaretColor(new Color(0, 0, 0));
+				remoteAdressField.setToolTipText("Enter IP adress of interlocutor");
+				remoteAdressField.setForeground(new Color(0, 0, 0));
+				remoteAdressField.setBounds(420, 51, 150, 22);
+				topPanel.add(remoteAdressField);
+				remoteAdressField.setColumns(12);
+				
+				JLabel remoteNickLabel = new JLabel("Remote Login");
+				remoteNickLabel.setLabelFor(remoteNickField);
+				remoteNickLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				remoteNickLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+				remoteNickLabel.setForeground(new Color(255, 153, 0));
+				remoteNickLabel.setBounds(306, 16, 110, 22);
+				topPanel.add(remoteNickLabel);
+				
+				JLabel remoteAdressLabel = new JLabel("Remote Adress");
+				remoteAdressLabel.setLabelFor(remoteAdressField);
+				remoteAdressLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				remoteAdressLabel.setForeground(new Color(255, 153, 0));
+				remoteAdressLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+				remoteAdressLabel.setBounds(306, 50, 110, 22);
+				topPanel.add(remoteAdressLabel);
+				
+				localNickField = new JTextField();
+				localNickField.setToolTipText("Enter your nick");
+				localNickField.setCaretColor(new Color(0, 0, 0));
+				localNickField.setForeground(new Color(0, 0, 0));
+				localNickField.setBounds(125, 17, 150, 22);
+				topPanel.add(localNickField);
+				localNickField.setColumns(15);
+				
+				JLabel localNickLabel = new JLabel("Local Login");
+				localNickLabel.setLabelFor(localNickField);
+				localNickLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+				localNickLabel.setForeground(new Color(255, 153, 0));
+				localNickLabel.setHorizontalAlignment(SwingConstants.CENTER);
+				localNickLabel.setBounds(32, 16, 85, 22);
+				topPanel.add(localNickLabel);
+				
+				applyButton = new JButton("Apply");
+				applyButton.setRequestFocusEnabled(false);
+				applyButton.setToolTipText("Click to apply your nick");
+				applyButton.setBackground(new Color(255, 215, 0));
+				applyButton.setFont(new Font("Broadway", Font.PLAIN, 14));
+				applyButton.setForeground(new Color(0, 51, 255));
+				applyButton.setActionCommand("Apply");
+				applyButton.setBounds(77, 51, 85, 22);
+				topPanel.add(applyButton);
+				
+				topSeparator = new JSeparator();
+				topSeparator.setForeground(new Color(105, 105, 105));
+				topSeparator.setBounds(0, 99, 790, 4);
+				topPanel.add(topSeparator);
+				
+				JPanel mainPanel = new JPanel();
+				mainPanel.setBounds(2, 102, 790, 370);
+				frame.getContentPane().add(mainPanel);
+				mainPanel.setLayout(null);
+				
+				sendButton = new JButton("Send");
+				sendButton.setRequestFocusEnabled(false);
+				sendButton.setFont(new Font("Broadway", Font.PLAIN, 14));
+				sendButton.setForeground(new Color(0, 51, 255));
+				sendButton.setToolTipText("Click to send message");
+				sendButton.setBackground(new Color(255, 215, 0));
+				sendButton.setBounds(644, 332, 98, 26);
+				mainPanel.add(sendButton);
+				
+				messageField = new JTextField();
+				messageField.setCaretColor(new Color(0, 0, 0));
+				messageField.setBounds(43, 332, 575, 26);
+				mainPanel.add(messageField);
+				messageField.setColumns(10);
+				
+				dialogArea = new JTextArea();
+				dialogArea.setEnabled(false);
+				dialogArea.setForeground(new Color(0, 0, 0));
+				dialogArea.setBounds(43, 12, 699, 288);
+				mainPanel.add(dialogArea);
+				
+				bottomSeparator = new JSeparator();
+				bottomSeparator.setForeground(new Color(105, 105, 105));
+				bottomSeparator.setBounds(0, 318, 790, 2);
+				mainPanel.add(bottomSeparator);
 				
 			}
 		});	
 	}
-
-public MainForm() throws IOException {
-	Toolkit kit = Toolkit.getDefaultToolkit();
-	Dimension screenSize = kit.getScreenSize();
-	int screenHeight = screenSize.height;
-	int screenWidth = screenSize.width;
-	
-	frame = new JFrame();
-	frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.LINE_AXIS));
-	frame.setBounds(screenWidth/6, screenHeight/6, DEFAULT_WIDTH, DEFAULT_HEIGHT);
-	Image img = new ImageIcon("ChatApp.png").getImage();
-	frame.setIconImage(img);
-	
-	frame.getContentPane().setLayout(new BorderLayout());
-    JPanel messagePanel = new JPanel();
-    messagePanel.setLayout(new GridLayout(1, 2));
-    frame.add(messagePanel, BorderLayout.SOUTH);
-    
-    sendButton = new JButton("Send");
-    sendButton.setPreferredSize(new Dimension(10,25));
-    sendButton.setMaximumSize(new Dimension(10,25));
-    sendButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
-	sendButton.setEnabled(false);
-    
-    disconnectButton = new JButton("Disconnect");
-    disconnectButton.setPreferredSize(new Dimension(100,25));
-    disconnectButton.setMaximumSize(new Dimension(100,25));
-    disconnectButton.setAlignmentX(Component.RIGHT_ALIGNMENT);
-   	disconnectButton.setEnabled(false);
-    
-    connectButton = new JButton("Connect");
-    connectButton.setPreferredSize(new Dimension(100,25));
-    connectButton.setMaximumSize(new Dimension(100,25));
-    
-    messagePanel.add(new JTextField(100));
-    messagePanel.add(sendButton);
-    
-    textField = new JTextField();
-    textField.setMaximumSize(new Dimension(50, 25));
-    textField.setPreferredSize(new Dimension(50, 25));
-    frame.add(textField, BorderLayout.CENTER);
-    
-    JPanel topPanel = new JPanel();
-    topPanel.setLayout(new GridLayout(2, 2));
-    
-    JPanel loginPanel = new JPanel();
-    loginPanel.add(new JLabel("local nick"));
-    loginPanel.add(new JTextField(10));
-    topPanel.add(loginPanel);
-    
-    JPanel nickPanel = new JPanel();
-    nickPanel.add(new JLabel("remote login"));
-    nickPanel.add(new JTextField(10));
-    nickPanel.add(disconnectButton);
-    topPanel.add(nickPanel);
-    
-    final JButton applyButton = new JButton("Apply");
-    applyButton.setMaximumSize(new Dimension(25,25));
-    applyButton.setPreferredSize(new Dimension(25,25));
-    applyButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-    topPanel.add(applyButton);
-    
-    JPanel connectionPanel = new JPanel();
-    connectionPanel.add(new JLabel("remote address"));
-    
-    connectionPanel.add(new JTextField(10));
-    connectionPanel.add(connectButton);
-    topPanel.add(connectionPanel);
-    frame.add(topPanel, BorderLayout.NORTH);
-    
-	disconnectButton.addActionListener(new ActionListener() {
-
-		public void actionPerformed(ActionEvent e) {
-
-			try {
-				if (connection != null) {
-
-					connection.disconnect();
-					forDisconnect();
-					connection = null;
-					commandLT.stop();
-				}
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-
-		}
-
-	});
-
-	connectButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			if (remoteAddressField.getText() != "") {
-				String login;
-				login = nickField.getText();
-				caller = new Caller(login, remoteAddressField.getText());
-				try {
-					connection = caller.call();
-					if (connection != null) {
-						commandLT = new CommandListenerThread(connection);
-						commandLT.start();
-						connection.sendNickHello("1", nickField.getText());
-						forConnect();
-					} else{
-						JOptionPane.showMessageDialog(null,
-								"Couldn't connect this ip ");
-					}
-				} catch (UnsupportedEncodingException e1) {
-
-					e1.printStackTrace();
-				} catch (IOException e1) {
-
-					e1.printStackTrace();
-				}
-
-			}
-		}
-	});
-	messageArea.addKeyListener(new KeyAdapter() {
-		public void keyPressed(KeyEvent e) {
-			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-				connection.sendMessage(messageArea.getText());
-				messageArea.setText("");
-				messageArea.setCursor(new Cursor(0));
-
-			} else {
-				JOptionPane.showMessageDialog(null, "You must write remote address");
-			}
-		}
-	});
-	sendButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			if (!messageArea.getText().equals("")) {
-				connection.sendMessage(messageArea.getText());
-				messageArea.setText("");
-
-			}
-		}
-
-	});
-	applyButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			String login;
-			if (nickField.getText().equals("")) {
-				login = "unnamed";
-			} else
-				login = nickField.getText();
-			boolean isCorrectLogin=false;
-			for (int i=0; i<login.toCharArray().length;i++)
-				if (login.toCharArray()[i]!=' '){
-						isCorrectLogin=true;
-						break;
-				}
-			if (!isCorrectLogin){
-				login ="unnamed";
-			}
-			while (login.charAt(0)==' ')
-				login=login.substring(1);
-			nickField.setText(login);
-			nickField.setEnabled(false);
-			try {
-				connection.isOpen();
-				callLT = new CallListenerThread();
-				callLT.start();
-				commandLT = new CommandListenerThread();
-				applyButton.setEnabled(false);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-
-			callLT.setLocalNick(login);
-		}
-	});
-}
-
-void forDisconnect() {
-	sendButton.setEnabled(false);
-	remoteLoginField.setText("");
-	messageArea.setEnabled(false);
-	disconnectButton.setEnabled(false);
-	connectButton.setEnabled(true);
-	remoteAddressField.setText("");
-	remoteAddressField.setEnabled(true);
-}
-
-void forConnect() throws IOException {
-	sendButton.setEnabled(true);
-	connectButton.setEnabled(false);
-	messageArea.setEnabled(true);
-	disconnectButton.setEnabled(true);
-	remoteAddressField.setEnabled(false);
-}
+	public Color getConnectButtonBackground() {
+		return connectButton.getBackground();
+	}
+	public void setConnectButtonBackground(Color background) {
+		connectButton.setBackground(background);
+	}
 }
